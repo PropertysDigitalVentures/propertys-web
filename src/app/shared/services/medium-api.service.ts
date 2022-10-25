cef71c7ab332e8276604bf240b6d33e4a4a387a5
Stevie Starosciak <snstarosciak@gmail.com>
2022-10-25 11:55:25 -0400

    Stylistic fixes on pages

--- src/app/shared/services/medium-api.service.ts
+++ src/app/shared/services/medium-api.service.ts
@@ -16,39 +16,8 @@ export class MediumApiService {
     }
 
     async getMediumPosts() {
-        let data = [
-            {
-                "id": "b969ac62a46b",
-                "name": "About Medium",
-                "description": "What is this thing and how does it work?",
-                "url": "https://medium.com/about",
-                "imageUrl": "https://cdn-images-1.medium.com/fit/c/200/200/0*ae1jbP_od0W6EulE.jpeg"
-            },
-            {
-                "id": "b45573563f5a",
-                "name": "Developers",
-                "description": "Medium’s Developer resources",
-                "url": "https://medium.com/developers",
-                "imageUrl": "https://cdn-images-1.medium.com/fit/c/200/200/1*ccokMT4VXmDDO1EoQQHkzg@2x.png"
-            },
-            {
-                "id": "b969ac62a46b",
-                "name": "About Medium",
-                "description": "What is this thing and how does it work?",
-                "url": "https://medium.com/about",
-                "imageUrl": "https://cdn-images-1.medium.com/fit/c/200/200/0*ae1jbP_od0W6EulE.jpeg"
-            },
-            {
-                "id": "b45573563f5a",
-                "name": "Developers",
-                "description": "Medium’s Developer resources",
-                "url": "https://medium.com/developers",
-                "imageUrl": "https://cdn-images-1.medium.com/fit/c/200/200/1*ccokMT4VXmDDO1EoQQHkzg@2x.png"
-            }
-        ];
-        
         let result = await this.http.get(`${environment.netlifyFnServer}get-medium-posts`).toPromise();
-        console.log('result from medium', result);
-        return data;
+        console.log('result', result);
+        return result.data.splice(0, 4) || [];
     }
 }
